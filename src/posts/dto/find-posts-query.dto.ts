5798a52de02033aaeda1499406e4fec066a5d667
Bumang-Cyber <calmness0729@gmail.com>
2025-12-25 11:22:45 +0900

    feat: getPosts API 비로그인유저/로그인유저 통합

---
+++ src/posts/dto/find-posts-query.dto.ts
@@ -0,0 +1,67 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsInt, IsString, IsArray } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class FindPostsQueryDto {
+  @ApiProperty({
+    description: '그룹 아이디로 조회 시',
+    required: false,
+    type: Number,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  groupId?: number;
+
+  @ApiProperty({
+    description: '카테고리 아이디로 조회 시',
+    required: false,
+    type: Number,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  categoryId?: number;
+
+  @ApiProperty({
+    description: '태그 아이디로 조회 (중첩 가능)',
+    required: false,
+    type: [Number],
+    example: [1, 2, 3],
+  })
+  @IsOptional()
+  @IsArray()
+  tagIds?: number[];
+
+  @ApiProperty({
+    description: '페이지당 게시글 수',
+    required: false,
+    default: 12,
+    type: Number,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  pageSize?: number = 12;
+
+  @ApiProperty({
+    description: '페이지 번호',
+    required: false,
+    default: 1,
+    type: Number,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  pageIndex?: number = 1;
+
+  @ApiProperty({
+    description: '포스트 타입 (dev, life)',
+    required: false,
+    type: String,
+    example: 'dev',
+  })
+  @IsOptional()
+  @IsString()
+  type?: string;
+}
